b53e71db9d37d6bfb4b5893500dfad66714f7d55
Pavel Zharov <pavel@prna.ru>
2021-10-19 09:54:40 +0700

    feat: :card_file_box: connect to mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening port ${PORT}`);
