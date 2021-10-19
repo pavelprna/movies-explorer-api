a0e181c543ff27ae852520de6793f6bf093bd529
Pavel Zharov <pavel@prna.ru>
2021-10-19 09:09:14 +0700

    feat: :sparkles: create express server

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening port ${PORT}`);
+});
