b098c9852fd5fcd7266bf49a4f90f0e0fe74623d
Pavel Zharov <pavel@prna.ru>
2021-10-20 19:21:39 +0700

    feat: :sparkles: add controllers for 'user/me' route

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 
 const BadRequestError = require('../errors/bad-request-error');
+const NotFoundError = require('../errors/not-found-error');
 
 const getUser = (req, res, next) => {
   User.findById(req.params.id)
@@ -8,12 +9,41 @@ const getUser = (req, res, next) => {
       if (user) {
         res.send(user);
       } else {
-        throw new BadRequestError('Нет пользователя с таким id');
+        throw new BadRequestError('Нет пользователя с таким _id');
+      }
+    })
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        throw new BadRequestError('Неверно указан _id пользователя');
+      } else {
+        next(error);
+      }
+    })
+    .catch(next);
+};
+
+const editUser = (req, res, next) => {
+  const { name, email, password } = req.body;
+  User.findByIdAndUpdate({ _id: req.user._id },
+    { name, email, password },
+    { new: true, runValidators: true })
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        throw new NotFoundError('Нет пользователя с таким _id');
+      }
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        throw new BadRequestError('Переданы некорректные данные при обновлении профиля');
+      } else {
+        next(error);
       }
     })
     .catch(next);
 };
 
 module.exports = {
-  getUser,
+  getUser, editUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { getUser } = require('../controllers/user');
+const { getUser, editUser } = require('../controllers/user');
 
 router.get('/me', getUser);
+router.patch('/me', editUser);
 
 module.exports = router;
