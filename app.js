63199a2347523571192d47d2ccf853034c8add71
Pavel Zharov <pavel@prna.ru>
2021-10-23 22:23:22 +0700

    feat: :sparkles: add cors rules

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUserValidator, loginValidator } = require('./middlewares/validation');
 const { createUser, login, signout } = require('./controllers/user');
@@ -13,6 +16,27 @@ const error = require('./middlewares/error');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const allowList = [
+  'https://prna.nomoredomains.club',
+  'http://prna.nomoredomains.club',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (allowList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,6 +62,7 @@ app.get('*', () => {
 });
 
 app.use(errorLogger);
+app.use(errors());
 app.use(error);
 
 app.listen(PORT, () => {
